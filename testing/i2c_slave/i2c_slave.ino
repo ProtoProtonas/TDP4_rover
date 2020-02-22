ef6e9e5e8399f6c0e3d5fbfae912d54dca477afc
Augustas Sereika <augustasereika@gmail.com>
2020-02-22 15:17:20 +0000

    created testing and main folders

---
+++ testing/i2c_slave/i2c_slave.ino
@@ -0,0 +1,13 @@
+#include "Wire.h"
+
+#define address 5
+
+void setup() {
+  // put your setup code here, to run once:
+  Wire.begin(address);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
